e954f8c19c097709a9d85ec475a3b7dca74a957d
bastien-viatge <bastien.viatge@insa-lyon.fr>
2014-12-05 10:36:23 +0100

    changement de nom, pour Lilian rpz, aiiiiie

---
+++ sched_PPF.h
@@ -0,0 +1,61 @@
+#define STACK_SIZE 2048
+#define NULL 0
+
+
+typedef enum State State;
+
+typedef void (*funct_t)(void);
+
+
+
+enum State
+{
+	NEW,WAITING,RUNNING,TERMINATED,JUST_WAITING
+};
+
+struct ctx_s{
+	void* sp;
+	void* pc;
+	void* init_sp;
+};
+
+struct pcb_s{
+	struct pcb_s* next_pcb;
+	void* pcb_args;
+	funct_t pcb_f;
+	struct ctx_s* pcb_ctx;
+	State state;
+	void* address;
+	int priority;
+};
+
+struct chained_list{
+	int priority;
+	struct pcb_s* first_pcb;
+	struct pcb_s* last_pcb;
+};
+
+struct pcb_s* current_process;
+
+struct chained_list* listeCL [4];
+struct chained_list priority0;
+struct chained_list priority1;
+struct chained_list priority2;
+struct chained_list priority3;
+
+
+void start_sched_PPF();
+
+void init_pcb(struct pcb_s* context, funct_t f, unsigned int size, void* args , int priority);
+
+void init_ctx(struct ctx_s* context, funct_t f, unsigned int size);
+
+void create_process(funct_t f, void* args, unsigned int stack_size,int priority);
+
+void addToChainedList(struct pcb_s* new_pcb, struct chained_list* CL);
+
+void start_current_process();
+
+void terminate_process();
+
+void elect();
